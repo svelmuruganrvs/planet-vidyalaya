dfab0f4e746d9b86153e002159c9dd7505a43588
Dakshina Murthy <dkmurthy9@gmail.com>
2010-09-07 17:24:17 +0530

    Generating Google CSE annotations

--- deploy/refresh_feeds.sh
+++ deploy/refresh_feeds.sh
@@ -28,4 +28,7 @@ done < subscriptions.csv
 
 python /var/www/venus/planet.py $CONFIG
 
+# Generate annotations file for Google CSE
+wget -O google_cse_annotations.xml "http://www.google.com/cse/tools/makeannotations?url=blogs.rmv.ac.in%2Fopml.xml&label=_cse_-c04vdhk8jg"
+
 mv $CONFIG.orig $CONFIG
