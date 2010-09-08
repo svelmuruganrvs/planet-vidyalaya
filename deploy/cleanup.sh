542bce5a6f11e2b0c91e27704ec48d803a37b8b3
Dakshina Murthy <dkmurthy9@gmail.com>
2010-09-08 22:54:12 +0530

    Adding mtime to css to invalidate browser cache in case of changes

--- deploy/cleanup.sh
+++ deploy/cleanup.sh
@@ -9,6 +9,10 @@ mv $CONFIG.orig $CONFIG
 sed -i s/page1.html//g page*html
 cp page1.html index.html
 
+# Add modification time to css so that browser can request fresh copy in case of changes
+mtime=`ls -lc --time-style=+%Y%m%d%H%M%S media/css/index.css | cut -d" " -f6`
+sed -i s/index.css/index.css?mtime=$mtime/g *.html
+
 rm config/page?.html.dj
 rm config/page??.html.dj
 
